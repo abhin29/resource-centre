2846821d815c0a6aa2c39b0693f4d01f45a30aca
keyence-hub <65643806+keyence-hub@users.noreply.github.com>
2020-06-01 14:05:09 +0530

    Create playlist-SR.js

---
+++ playlist/playlist-SR.js
@@ -0,0 +1,38 @@
+let data = [
+  {
+    title: "SR-1000",
+    headings: [
+      {
+        subtitle: "SR Vs Handheld Reading Codes on Big Part",
+        description: "",
+        link: "https://youtu.be/UFmKphQMY3I"
+      },
+      {
+        subtitle: "SR Vs Handheld Reading Multiple codes",
+        description: "",
+        link: "https://youtu.be/diLHiXCs6Jc"
+       },
+      {
+        subtitle: "SR Vs Handheld Reading Curved code",
+        description: "",
+        link: "https://youtu.be/6XbJ7nRFO6Q"
+       },
+      {
+        subtitle: "SR Vs Handheld Reading through Dirty Plastic bags",
+        description: "",
+        link: "https://youtu.be/i9jKhO8b5q0"
+       },
+      {
+        subtitle: "SR Vs Handheld Reading through Plastic bags",
+        description: "",
+        link: "https://youtu.be/NNI2itxdb-k"
+       }
+    ],
+    disabled_subtitles: [
+      "IV2",
+      "IV",
+      "",
+      
+    ]
+  }
+]
