3b3e1af662800afd32f8642aaaa0f4400b21d03b
keyence-hub <65643806+keyence-hub@users.noreply.github.com>
2020-06-01 14:03:38 +0530

    Create playlist.js

---
+++ playlist/playlist.js
@@ -0,0 +1,107 @@
+let data = [
+  {
+    title: "Vision sensors",
+    headings: [
+      {
+        subtitle: "IV2",
+        description: "Under Development",
+        link: ""
+      },
+      {
+        subtitle: "IV",
+        description: "Under Development",
+        link: ""
+       }
+    ],
+    disabled_subtitles: [
+      "IV2",
+      "IV",
+      "",
+      
+    ]
+  },
+  {
+    title: "Auto ID",
+    headings: [
+      {
+        subtitle: "SR",
+        description: "Code reader",
+        link: "../playlist/playlist-SR.html"
+      }
+    ],
+    disabled_subtitles: [
+      "SJ",
+      "",
+      "",
+      
+    ]
+  },
+  {
+    title: "Static Eliminator",
+    headings: [
+      {
+        subtitle: "SJ",
+        description: "Static Eliminator",
+        link: ""
+      }
+    ],
+    disabled_subtitles: [
+      "SJ",
+      "",
+      "",
+      
+    ]
+  },
+  {
+    title: "Application Sensors-1",
+    headings: [
+      {
+        subtitle: "GT",
+        description: "LVDT Contact sensor",
+        link: ""
+      },
+      {
+        subtitle: "IL",
+        description: "Laser Displacement sensor",
+        link: ""
+      },
+      {
+        subtitle: "IG",
+        description: "Thrubeam sensor",
+        link: ""
+      }
+    ],
+     disabled_subtitles: [
+      "GT",
+      "IL",
+      "IG",
+      
+    ]
+  },
+  {
+    title: "Application Sensors-2",
+    headings: [
+      {
+        subtitle: "EX",
+        description: "Proximity Vibration sensor",
+        link: ""
+      },
+      {
+        subtitle: "GA",
+        description: "Shock sensor",
+        link: ""
+      },
+      {
+        subtitle: "FW",
+        description: "Ultrasonic Distance sensor",
+        link: ""
+      },
+    ],
+    disabled_subtitles: [
+      
+      "EX",
+      "GA",
+      "FW",
+    ]
+  }
+];
