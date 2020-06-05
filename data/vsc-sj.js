53f523252db6b927bc6c39c4ec7e66af6a7ed858
Glitch (git-testing-site) <none>
2020-06-05 05:05:24 +0000

    🛢🏣 Updated with Glitch

---
+++ data/vsc-sj.js
@@ -0,0 +1,16 @@
+let data = [
+   {
+    title: "Introduction",
+    headings: [
+      {
+        subtitle: "General Introduction",
+        description: "",
+        link: "https://keyencein.sharepoint.com/sites/KIN_ALL2/_layouts/15/Doc.aspx?OR=teams&action=edit&sourcedoc={DDF619C1-DF27-4A0D-849D-C73A3090B748}"
+      }
+    ],
+    disabled_subtitles: [
+      "",
+      "",
+    ]
+  }
+];
