fc41235a878e2af0ed4be393db72b752edaefe2a
keyence-hub <65643806+keyence-hub@users.noreply.github.com>
2020-06-05 11:13:24 +0530

    Update vsc-iv2.js

--- data/vsc-iv2.js
+++ data/vsc-iv2.js
@@ -70,7 +70,7 @@ let data = [
       {
         subtitle: "Additional learning (AI) ",
         description: "Procedure of Additional Learning",
-        link: "https://eqc028ozal.eq.webcdn.stream.ne.jp/www50/eqc028ozal/jmc_pub/jmc_swf/player/player_3_2_0/onetag_t3.html?b=eqc028ozal.eq.webcdn.stream.ne.jp/www50/eqc028ozal/jmc_pub/jmc_swf/player/&c=MjQxMQ==&m=MjI5MA==&s=%7Bdq%3A%200%2Chp%3A%20405%2Cht%3A%20100%2Cil%3A%20off%2Cmdq%3A%200%2Cpr%3A%200.5%2C0.7%2C1.2%2C1.5%2C2.0%2Csn%3A%20%2Ctg%3A%20off%2Cti%3A%20off%2Cskb%3A%20off%2Cip%3A%20on%2Cis%3A%20off%2Crp%3A%20off%2Cwp%3A%20720%2Cwt%3A%20178%7D&base=eqc028ozal.eq.webcdn.stream.ne.jp/www50/eqc028ozal/jmc_pub/jmc_swf/player/&contract_id=2411&meta_id=2290&responsive=off&default_quality=0&lightboxTextLink=&liveFeedSelect=0&eqplcid=ST0d2Ph4SHNufDG2WntUDyT5SNX8ueUG&eqplsid=9VOS50LQNWbumcFiI2ieccFnpOfz60F5&parent_url=https%3A%2F%2Fwww.keyence.com%2Fsupport%2Fuser%2Fiv2%2Fvideo%2F&domain=https%3A%2F%2Fwww.keyence.com%2Fsupport%2Fuser%2Fiv2%2Fvideo%2F&ref=https%3A%2F%2Fwww.keyence.com%2Fsupport%2Fuser%2Fiv2%2F&tagType=player_3_2_0&isLightbox=true&hostCheckResult=ok&tk=R5ZApEIsOwvbfg-TsLbvTQoJehHSQgsT2UOVk5&aes_token=&isAuthPassed=ZmFsc2UyMDIwNTE5&o=&ot=&vc=ICG3&inCookieVolume=mute&useShortcutKey=on&"
+        link: "https://youtu.be/WmX8ogixiUk"
       },
       {
         subtitle: " Web Monitor ",
