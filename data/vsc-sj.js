1d138d9fa983a6c5f7238705b61e95eb73e8ae0e
Glitch (git-testing-site) <none>
2020-06-16 03:27:54 +0000

    👡👌 Updated with Glitch

--- data/vsc-sj.js
+++ data/vsc-sj.js
@@ -3,12 +3,12 @@ let data = [
     title: "Introduction",
     headings: [
       {
-        subtitle: "General Introduction",
+        subtitle: "Static Eliminator - SJ",
         description: "",
         link: "https://keyencein.sharepoint.com/sites/KIN_ALL2/_layouts/15/Doc.aspx?OR=teams&action=edit&sourcedoc={734D476E-FE75-4082-8D28-F407F0FE6CB4}"
       },
       {
-        subtitle: "General Introduction",
+        subtitle: "Electrostatic sensor - SK",
         description: "",
         link: "https://keyencein.sharepoint.com/sites/KIN_ALL2/_layouts/15/Doc.aspx?OR=teams&action=edit&sourcedoc={21A80882-FBDB-4236-AF98-C9779D948DDC}"
       }
