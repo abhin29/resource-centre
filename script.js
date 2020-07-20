e4337f684d6eddb5f7a67b00960b4588279d11d7
Adarsh Naidu <naidu.adarsh2511@gmail.com>
2020-07-20 10:54:39 +0530

    adding collapsible script and style

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 data.forEach(element => {
-  $section = $(`<div class="section"><h2>${element.title}</h2></div>`);
-  $cardsContainer = $(`<div class="cards-container"></div>`);
+  if(element.toggle){
+    $section = $(`<div class="section"><h2 class="toggle">${element.title}</h2></div>`);
+    $cardsContainer = $(`<div class="inner cards-container"></div>`);
+  } else {
+    $section = $(`<div class="section"><h2>${element.title}</h2></div>`);
+    $cardsContainer = $(`<div class="cards-container"></div>`);
+  }
   element.headings.forEach(heading => {
     let html
     if (element.disabled_subtitles && element.disabled_subtitles.includes(heading.subtitle)){
@@ -12,4 +17,19 @@ data.forEach(element => {
   });
   $section.append($cardsContainer);
   $(".container").append($section);
+});
+
+$('.toggle').click(function(e) {
+  e.preventDefault();
+  var $this = $(this);
+  
+  if ($this.next().hasClass('show')) {
+      $this.next().removeClass('show');
+      $this.next().slideUp(350);
+  } else {
+      $this.next().toggleClass('show');
+      $this.next().slideToggle(350, () => {
+          $this.next().css('display', 'flex')
+      });
+  }
 });
